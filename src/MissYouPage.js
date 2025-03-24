e58211dd8ae56f07d09b653771b56001f96ae703
michael <mikeramage.ed@gmail.com>
2025-03-24 05:40:54 -0400

    copy paste trash stuff

---
+++ src/MissYouPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./NotePage.css"; // Optional for additional styling if needed
+
+const MissYouPage = () => {
+  return (
+    <div className="note-container">
+      <h1>aye</h1>
+      <h2>03/24/2025, 5:40am EST</h2>
+      <p>
+        you prob don't think it, but i miss you 🫰🙄, even if i am not messaging you...
+      </p>
+    </div>
+  );
+};
+
+export default MissYouPage;
